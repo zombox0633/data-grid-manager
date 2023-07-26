4e18dd4e291896059cf4f0e994a6a10380c85c9f
Zombox <ghostzb@outlook.com>
2023-07-26 20:09:24 +0700

    Initial project

---
+++ README.md
@@ -0,0 +1,25 @@
+css variable
+
+ESLint a11y
+
+**Test
+unit test - vitest
+test @testing-library/react
+@testing-library/userEvent
+
+theme color
+#fff8e8
+
+//theme 1
+#D9181F
+#FDF4EE
+#024B5F
+#80B3C2
+
+//theme 2
+#1C1B19
+#2E2619
+#7B7966
+#8DB9A5
+#FAF6EF
+#DBC066
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+//https://www.behance.net/gallery/158625569/Login-Page?tracking_source=search_projects|web+design+login
+//https://www.behance.net/gallery/163670027/KOELD?tracking_source=search_projects
+
+function App() {
+
+  return (
+    <>
+      <span role="img" className=" text-lg">😊</span>
+    </>
+  )
+}
+
+export default App
