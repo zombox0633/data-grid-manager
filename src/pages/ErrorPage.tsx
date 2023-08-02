f3f97633cce3a906a5aa23230d3e2932898aa5e7
Zombox <ghostzb@outlook.com>
2023-08-02 21:26:00 +0700

    Add ErrorPage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function ErrorPage() {
   return (
-    <div>ErrorPage</div>
-  )
+    <div className=" flex flex-col justify-between h-[100vh] mx-[5%] py-[5%] overflow-hidden">
+      <span className="h-30 font-semibold text-lg">404</span>
+      <div>
+        <h1 className="my-5">Page not found</h1>
+        <p className="font-semibold text-xl">
+          The page you are looking for doesn't exist.
+        </p>
+      </div>
+      <div className="h-30"></div>
+    </div>
+  );
 }
 
-export default ErrorPage
+export default ErrorPage;
